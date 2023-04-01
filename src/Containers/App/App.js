96aad12b231371e4d56552a47ebf35aceb52897e
Heather Hartley <hlhartley@gmail.com>
2023-04-01 11:54:00 -0600

    Add stay function logic

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -20,7 +20,8 @@ function App() {
   const getRandomCards = (numCards) => {
     const randomCards = [];
     for (let i=0; i<numCards; i++) {
-      const randomCard = cards[Math.floor(Math.random() * (Math.floor(51) - 0) + 0)];
+      const nonSelectedCards = cards.filter((card) => !card.isSelected);
+      const randomCard = nonSelectedCards[Math.floor(Math.random() * (Math.floor(nonSelectedCards.length) - 0) + 0)];
       randomCard.isSelected = true;
       randomCards.push(randomCard);
     }
@@ -28,35 +29,64 @@ function App() {
   }
 
   const dealCards = () => {
-    const [card1, card2, card3, card4] = getRandomCards(4);
+    const [card1, card2, card3] = getRandomCards(3);
     setPlayerCards(p => [...p, card1, card2]);
-    setDealerCards(p => [...p, card3, card4]);
+    setDealerCards(p => [...p, card3]);
     setPlayerPoints(card1.value + card2.value);
-    setDealerPoints(card3.value + card4.value);
+    setDealerPoints(card3.value);
   }
 
-  const hit = (value) => {
-    turn === 'Player' ? setPlayerPoints(value) : setDealerPoints(value);
+  const hit = () => {
+    const [randomCard] = getRandomCards(1);
+    if (turn === 'Player') {
+      setPlayerCards(p => [...p, randomCard]);
+      setPlayerPoints(playerPoints + randomCard.value);
+    } else {
+      setDealerCards(p => [...p, randomCard]);
+      setDealerPoints(dealerPoints + randomCard.value);
+    }
+  }
+
+  const stay = () => {
+    setTurn('Dealer');
+
+    let points = dealerPoints;
+    for (let i=0; points < 17; i++) {
+      const [randomCard] = getRandomCards(1);
+      setDealerCards(p => [...p, randomCard]);
+      points += randomCard.value;
+
+      if (points > 16) {
+        setDealerPoints(points);
+        return;
+      }
+
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <div>Turn: {turn}</div>
+      </header>
+      <section className="App-body">
         <div>
-          Dealer: {dealerPoints}
           <Cards
             cards={dealerCards}
           />
+          Dealer: {dealerPoints}
         </div>
         <div>
-          Player : {playerPoints}
           <Cards
             cards={playerCards}
           />
+          Player : {playerPoints}
         </div>
-        <button onClick={() => hit(playerPoints + 1)}>Increment Value</button>
-      </header>
+        <div>
+          <button onClick={() => hit()}>Hit</button>
+          <button onClick={() => stay()}>Stay</button>
+        </div>
+      </section>
     </div>
   );
 }
