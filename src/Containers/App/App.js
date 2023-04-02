a98957f7dd7ad2d93de75fd59aece2aadf17ebf8
Heather Hartley <hlhartley@gmail.com>
2023-04-01 18:49:45 -0600

    Revert back to original dataset

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -11,28 +11,22 @@ function App() {
   const [dealerCards, setDealerCards] = useState([]);
   const [turn, setTurn] = useState('Player');
   const [cards, setCards] = useState(deckOfCards);
-  const [deckId, setDeckId] = useState();
 
   useEffect(() => {
     if (playerCards.length === 0) {
-      const fetchNewDeck = async() => {
-        const response1 = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-        const result1 = await response1.json();
-        setDeckId(result1.deck_id);
-        const response2 = await fetch(`https://deckofcardsapi.com/api/deck/${result1.deck_id}/draw/?count=3`);
-        const result2 = await response2.json();
-        dealCards(result2.cards);
-      }
-      fetchNewDeck();
+      dealCards();
     }
-  }, [playerCards, dealerCards])
+  }, [playerCards])
 
-  const getRandomCards = async (deckId, numCards) => {
-    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numCards}`)
-    if (response.status === 200) {
-      const result = await response.json();
-      return result;
+  const getRandomCards = (numCards) => {
+    const randomCards = [];
+    for (let i=0; i<numCards; i++) {
+      const nonSelectedCards = cards.filter((card) => !card.isSelected);
+      const randomCard = nonSelectedCards[Math.floor(Math.random() * (Math.floor(nonSelectedCards.length) - 0) + 0)];
+      randomCard.isSelected = true;
+      randomCards.push(randomCard);
     }
+    return randomCards;
   }
 
   const newGame = () => {
@@ -48,43 +42,39 @@ function App() {
     dealCards();
   }
 
-  const dealCards = async (cards) => {
-    const [card1, card2, card3] = cards;
+  const dealCards = () => {
+    const [card1, card2, card3] = getRandomCards(3);
     setPlayerCards(p => [...p, card1, card2]);
     setDealerCards(p => [...p, card3]);
-    setPlayerPoints(parseInt(card1.value) + parseInt(card2.value));
-    setDealerPoints(parseInt(card3.value));
-    if (parseInt(card1.value) + parseInt(card2.value) === 21) {
+    setPlayerPoints(card1.value + card2.value);
+    setDealerPoints(card3.value);
+    if (card1.value + card2.value === 21) {
       setWinner('Player');
     }
   }
 
-  const hit = async () => {
-    const result = await getRandomCards(deckId, 1);
-    const cards = await result.cards;
-    const [card1] = cards;
+  const hit = () => {
+    const [randomCard] = getRandomCards(1);
     if (turn === 'Player') {
-      setPlayerCards(p => [...p, card1]);
-      setPlayerPoints(playerPoints + parseInt(card1.value));
-      if (playerPoints + parseInt(card1.value) > 21) {
+      setPlayerCards(p => [...p, randomCard]);
+      setPlayerPoints(playerPoints + randomCard.value);
+      if (playerPoints + randomCard.value > 21) {
         setWinner('Dealer');
       }
     } else {
-      setDealerCards(p => [...p, card1]);
-      setDealerPoints(dealerPoints + parseInt(card1.value));
+      setDealerCards(p => [...p, randomCard]);
+      setDealerPoints(dealerPoints + randomCard.value);
     }
   }
 
-  const stay = async () => {
+  const stay = () => {
     setTurn('Dealer');
 
     let points = dealerPoints;
     while (points < 17) {
-      const result = await getRandomCards(deckId, 1);
-      const [randomCard] = result.cards;
+      const [randomCard] = getRandomCards(1);
       setDealerCards(p => [...p, randomCard]);
-      points += parseInt(randomCard.value);
-      console.log(points)
+      points += randomCard.value;
       if (points > 16) {
         setDealerPoints(points);
         determineWinner(points);
